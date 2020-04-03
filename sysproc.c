4285838b6c855649d99aa4266ef7124490e46832
Estefania <elopez17@hawk.iit.edu>
2020-04-03 15:35:56 -0500

    added r/w

--- sysproc.c
+++ sysproc.c
@@ -98,7 +98,54 @@ int sys_myV2p(void)
   argstr(0, &va);
   argint(1, &instr);
 
+
+  int physical_address;
+  pde_t *pgdir,*pgtab,*pde;
+  pgdir = myproc()->pgdir;
+
+    //must initialise pgdir
+
+    pde = &pgdir[PDX(va)];
+    if(*pde & PTE_P){
+    pgtab = (pte_t*)P2V(PTE_ADDR(*pde));
+    }
+    else
+    {
+    cprintf("\n PTE Not Present! - Invalid Virtual address\n");
+    return -1;
+    }
+    cprintf("\n ----------------- \n");
+    cprintf(" Page Directory Entry (PDE): %d\n",*pde);
+    cprintf(" PTE_P : %d\n",PTE_P);
+    cprintf("\n ----------------- \n");
+
+    if(instr){
+      if(*pde & PTE_W){
+        pte_t *pte;
+        pte = &pgtab[PTX(va)];
+       physical_address=(char*)V2P(PTE_ADDR(*pte));
+
+       cprintf(" --PHYSICAL ADDRESS-- %d\n",physical_address);
+      }
+      else
+      {
+        cprintf("error, not writable");
+      }
+      
+    }
+    else{
+      pte_t *pte;
+    pte = &pgtab[PTX(va)];
+    physical_address=(char*)V2P(PTE_ADDR(*pte));
+
+    cprintf(" --PHYSICAL ADDRESS-- %d\n",physical_address);
+    }
+
   
+
+    return 0;
+
+  /*
   pde_t *pde;
   pte_t *pgtab, *pgdir;
   pgdir = myproc()->pgdir;
@@ -119,6 +166,9 @@ int sys_myV2p(void)
 	cprintf("\n Invalid Virtual Address! \n");
 	return -1;
   }
-	
-  return 0;
+	*/
+
+
+
+  //return 0;
 }
